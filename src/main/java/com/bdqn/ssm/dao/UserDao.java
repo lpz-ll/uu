b7f0b06e7ef298281b2b7f3e335dddf8f5c7cd65
ll <w15236629034@163.com>
2019-11-25 21:38:08 +0800

    1

---
+++ src/main/java/com/bdqn/ssm/dao/UserDao.java
@@ -0,0 +1,26 @@
+package com.bdqn.ssm.dao;
+
+import com.bdqn.ssm.entity.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface UserDao {
+    //添加
+    @Insert("INSERT INTO user(name) VALUES(#{name})")
+    void add(String name);
+    //删除
+    @Delete("DELETE FROM user WHERE id = {id} ")
+    void del(Long id);
+    //修改
+    void update(User user);
+    //查询
+    User get(Long id);
+    //查询全部
+    List<User> list();
+}
